ad4bb7a9833d3244d43fcefffb2c449bb98d91f5
Chi Truong <vtchitruong@gmail.com>
2021-06-07 21:43:43 +0700

    modify QuickSort class

--- oop_sort.cpp
+++ oop_sort.cpp
@@ -29,7 +29,7 @@ public:
 class QuickSort : public SortBase
 {
 private:
-    void qsort(int l, int r);
+    void qsort(int left, int right);
 public:
     virtual void Sort(int *aa, int n);
 };
@@ -57,6 +57,39 @@ void SelectionSort::Sort(int *aa, int n)
             if (k != j) swap(i, k);
         }
     }
+}
+
+//-----------------------------------------
+void QuickSort::qsort(int left, int right)
+{
+    int x, l, r;
+    if (left < right)
+    {
+        x = a[left];
+        l = left;
+        r = right + 1;
+
+        do
+        {
+            ++l;
+            --r;
+
+            while (l < right && a[l] < x) ++l;
+            while (a[r] > x) --r;
+            
+            if (l < r) swap(l, r);
+        } while (l < r);
+
+        swap(left, r);
+        qsort(left, r - 1);
+        qsort(r + 1, right);
+    }
+}
+
+void QuickSort::Sort(int *aa, int n)
+{
+    SortBase::Sort(aa, n);
+    qsort(0, n - 1);
 }
 
 int main()
