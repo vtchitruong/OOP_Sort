73875e79344d5af6ffdb065840ed021418062804
Chi Truong <vtchitruong@gmail.com>
2021-06-08 12:07:50 +0700

    modify HeapSort class

--- oop_sort.cpp
+++ oop_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -54,7 +55,7 @@ void SelectionSort::Sort(int *aa, int n)
         for (int j = i + 1; j < n; ++j)
         {
             if (a[j] < a[k]) k = j;
-            if (k != j) swap(i, k);
+            if (k != j) Swap(i, k);
         }
     }
 }
@@ -77,10 +78,10 @@ void QuickSort::qsort(int left, int right)
             while (l < right && a[l] < x) ++l;
             while (a[r] > x) --r;
             
-            if (l < r) swap(l, r);
+            if (l < r) Swap(l, r);
         } while (l < r);
 
-        swap(left, r);
+        Swap(left, r);
         qsort(left, r - 1);
         qsort(r + 1, right);
     }
@@ -92,9 +93,83 @@ void QuickSort::Sort(int *aa, int n)
     qsort(0, n - 1);
 }
 
+//---------------------------
+void HeapSort::Shift(int i, int n)
+{
+    int l, r, k;
+    l = 2 * i;
+    r = l + 1;
+
+    if (l > n) return;
+
+    if (l == n)
+    {
+        if (a[i] < a[l]) Swap(i, l);
+        return;
+    }
+
+    if (a[l] > a[r])
+    {
+        k = l;
+    }
+    else
+    {
+        k = r;
+    }
+
+    if (a[i] >= a[k])
+    {
+        return;
+    }
+    else
+    {
+        Swap(i, k);
+        Shift(k, n);
+    }     
+}
+
+void HeapSort::Sort(int *aa, int n)
+{
+    SortBase::Sort(aa, n);
+
+    for (int i = n / 2; i > 0; --i)
+    {
+        Shift(i, n);
+    }
+
+    for (int i = n - 1; i > 1; --i)
+    {
+        Swap(1, i);
+        Shift(1, i - 1);
+    }
+}
+
 int main()
 {
+    int n = 10000;
+    int *arr;
+    arr = (int*) malloc((n+1) * sizeof(int));
 
+    SortBase *sb[3];
+    SelectionSort ss;
+    QuickSort qs;
+    HeapSort hs;
+
+    sb[0] = &ss;
+    sb[1] = &qs;
+    sb[2] = &hs;
+
+    srand(time(NULL));
+    for (int k = 0; k < 3; ++k)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            arr[i] = rand() % 10000 + 1;
+        }
+
+        
+        sb[k]->Sort(arr, n);
+    }
 
     return 0;
 }
